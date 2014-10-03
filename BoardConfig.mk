6febac4c334c81999357c7caf603c9d10ed7daa8
Ethan Chen <intervigil@gmail.com>
2014-10-03 14:29:18 -0700

    tomato: Use mkbootimg to create bootimage
    Change-Id: I5bdb3568f51b8712cf94bc7553ed263f79b8c59a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,7 @@ TARGET_CPU_VARIANT := cortex-a53
 TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
 
 # Kernel
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
