7a1f3358f98b8301a9e2620ed7207eecb1108880
Scott Mertz <scott@cyngn.com>
2014-10-15 12:03:07 -0700

    tomato: Add missing media targets
    Change-Id: If266d825077bcf8659e6c140c8fa46884a732e33

--- device.mk
+++ device.mk
@@ -121,15 +121,15 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml
 
-# OMX
 PRODUCT_PACKAGES += \
     libdashplayer \
-    libOmxVdec \
-    libOmxVenc \
     libOmxAacEnc \
     libOmxAmrEnc \
+    libOmxCore \
     libOmxEvrcEnc \
     libOmxQcelp13Enc \
+    libOmxVdec \
+    libOmxVenc \
     libstagefrighthw \
     qcmediaplayer
 
