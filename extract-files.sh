5258e409282a96e056199d4cc6c50d6af7444dfa
Scott Mertz <scott@cyngn.com>
2014-10-21 19:39:11 -0700

    tomato: separate qc blobs from vendor blobs
    Change-Id: I65af8c658c5f639130aba01f0c40d32442d9fa49

--- extract-files.sh
+++ extract-files.sh
@@ -20,37 +20,42 @@ else
   fi
 fi
 
+function extract() {
+    for FILE in `egrep -v '(^#|^$)' $1`; do
+      OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+      FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+      DEST=${PARSING_ARRAY[1]}
+      if [ -z $DEST ]
+      then
+        DEST=$FILE
+      fi
+      DIR=`dirname $DEST`
+      if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+      fi
+      # Try CM target first
+      if [ "$SRC" = "adb" ]; then
+        adb pull /system/$DEST $BASE/$DEST
+        # if file does not exist try OEM target
+        if [ "$?" != "0" ]; then
+            adb pull /system/$FILE $BASE/$DEST
+        fi
+      else
+        if [ -z $SRC/system/$DEST ]; then
+            echo ":: $DEST"
+            cp $SRC/system/$DEST $BASE/$DEST
+        else
+            echo ":: $FILE"
+            cp $SRC/system/$FILE $BASE/$DEST
+        fi
+      fi
+    done
+}
+
 BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
 rm -rf $BASE/*
 
-for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
-  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
-  FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
-  DEST=${PARSING_ARRAY[1]}
-  if [ -z $DEST ]
-  then
-    DEST=$FILE
-  fi
-  DIR=`dirname $DEST`
-  if [ ! -d $BASE/$DIR ]; then
-    mkdir -p $BASE/$DIR
-  fi
-  # Try CM target first
-  if [ "$SRC" = "adb" ]; then
-    adb pull /system/$DEST $BASE/$DEST
-    # if file does not exist try OEM target
-    if [ "$?" != "0" ]; then
-        adb pull /system/$FILE $BASE/$DEST
-    fi
-  else
-    if [ -z $SRC/system/$DEST ]; then
-        echo ":: $DEST"
-        cp $SRC/system/$DEST $BASE/$DEST
-    else
-        echo ":: $FILE"
-        cp $SRC/system/$FILE $BASE/$DEST
-    fi
-  fi
-done
+extract ../../$VENDOR/$DEVICE/proprietary-files-qc.txt $BASE
+extract ../../$VENDOR/$DEVICE/proprietary-files.txt $BASE
 
 ./setup-makefiles.sh
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -28,6 +28,32 @@ PRODUCT_COPY_FILES += \\
 EOF
 
 LINEEND=" \\"
+COUNT=`wc -l proprietary-files-qc.txt | awk {'print $1'}`
+DISM=`egrep -c '(^#|^$)' proprietary-files-qc.txt`
+COUNT=`expr $COUNT - $DISM`
+for FILE in `egrep -v '(^#|^$)' proprietary-files-qc.txt`; do
+  COUNT=`expr $COUNT - 1`
+  if [ $COUNT = "0" ]; then
+    LINEEND=""
+  fi
+  # Split the file from the destination (format is "file[:destination]")
+  OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+  if [[ ! "$FILE" =~ ^-.* ]]; then
+    FILE=`echo ${PARSING_ARRAY[0]} | sed -e "s/^-//g"`
+    DEST=${PARSING_ARRAY[1]}
+    if [ -n "$DEST" ]; then
+      FILE=$DEST
+    fi
+    echo "    $OUTDIR/proprietary/$FILE:system/$FILE$LINEEND" >> $MAKEFILE
+  fi
+done
+(cat << EOF) >> $MAKEFILE
+endif
+
+PRODUCT_COPY_FILES += \\
+EOF
+
+LINEEND=" \\"
 COUNT=`wc -l proprietary-files.txt | awk {'print $1'}`
 DISM=`egrep -c '(^#|^$)' proprietary-files.txt`
 COUNT=`expr $COUNT - $DISM`
@@ -48,7 +74,6 @@ for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
   fi
 done
 (cat << EOF) >> $MAKEFILE
-endif
 EOF
 
 (cat << EOF) > ../../../$OUTDIR/$DEVICE-vendor.mk
