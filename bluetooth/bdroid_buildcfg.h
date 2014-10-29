fe79b7e7b9737d991ef3de78a4c1d46105d3045a
Scott Mertz <scott@cyngn.com>
2014-10-28 17:57:58 -0700

    tomato: cleanup bluedroid config
    - Removed defines are the default value anyway
    - Use default disable timeout
    
    Change-Id: I9b2a5aa59697fa03b9acc21884c6b48f7b6e59e8

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,8 +18,5 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "Coolpad 8675"
-#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define BLUETOOTH_QCOM_SW TRUE
-#define BLUETOOTH_QCOM_LE_INTL_SCAN TRUE
-#define BTC_INCLUDED TRUE
 #endif
