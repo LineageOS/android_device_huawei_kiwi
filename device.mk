e2aeebcbb20bb0813e6797bfd426d556b8e6da4c
Steve Kondik <shade@chemlab.org>
2014-11-06 01:03:51 -0800

    tomato: Add thermal engine configuration
     * Modified set point to 75 degrees and hotplug temp to 95 degrees.
    
    Change-Id: I8e691f034d7fb52b1e9cfafec849c208ae77c7fd

--- device.mk
+++ device.mk
@@ -218,6 +218,10 @@ PRODUCT_EXTRA_RECOVERY_KEYS += \
 PRODUCT_PACKAGES += \
     sensors.msm8916
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:system/etc/thermal-engine.conf
+
 # Torch
 PRODUCT_PACKAGES += \
     Torch
