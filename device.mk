83f2aeb1fe2b174151d1c08a1f6e3a1ff0fad9b5
Steve Kondik <shade@chemlab.org>
2014-11-05 23:35:04 -0800

    tomato: ANT+ support
    Change-Id: I86b47cf7359cfddd03f1f035df59d1d2c18c5cbb

--- device.mk
+++ device.mk
@@ -76,6 +76,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml
 
+# ANT+
+PRODUCT_PACKAGES += \
+    AntHalService \
+    com.dsi.ant.antradio_library \
+    libantradio
+
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916
