b201e4ca5364fd081e3457a074b0a081adf4ac10
Ricardo Cerqueira <ricardo@cyngn.com>
2015-01-09 03:02:52 +0000

    tomato64: Load wifi as a module
    The uncompressed kernel is too big for the space we have right now,
    trim it down a bit
    
    Change-Id: Ide7c2ba84e0799c04a5fc14648587e6b963748a7

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -180,6 +180,11 @@ TARGET_USES_WCNSS_CTRL := true
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
+# The uncompressed arm64 is too large, split wifi for now
+ifeq ($(USE_64_BIT),true)
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WIFI_DRIVER_MODULE_NAME          := "wlan"
+endif
 
 # inherit from the proprietary version
 -include vendor/yu/tomato/BoardConfigVendor.mk
