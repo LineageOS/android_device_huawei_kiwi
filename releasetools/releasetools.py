212b1459b1949d0c7a384c50aedc1fe1a6ae632e
Ethan Chen <intervigil@gmail.com>
2015-01-12 19:41:34 -0800

    kiwi: Add assertions on TrustZone version
    Change-Id: I491e141b33b7009a84747aaf694e9d5a19eba903

---
+++ releasetools/releasetools.py
@@ -0,0 +1,30 @@
+# Copyright (C) 2016 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import re
+
+def FullOTA_Assertions(info):
+  AddTrustZoneAssertion(info, info.input_zip)
+
+def IncrementalOTA_Assertions(info):
+  AddTrustZoneAssertion(info, info.input_zip)
+
+def AddTrustZoneAssertion(info, input_zip):
+  android_info = info.input_zip.read("OTA/android-info.txt")
+  m = re.search(r'require\s+version-trustzone\s*=\s*(\S+)', android_info)
+  if m:
+    versions = m.group(1).split('|')
+    if len(versions) and '*' not in versions:
+      cmd = 'assert(kiwi.verify_trustzone(' + ','.join(['"%s"' % tz for tz in versions]) + ') == "1");'
+      info.script.AppendExtra(cmd)
