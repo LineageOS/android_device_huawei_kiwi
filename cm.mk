4ce7fb839e06850e2f19dd5d1eac704a9e14e0fa
Ethan Chen <intervigil@gmail.com>
2015-01-21 11:59:03 -0800

    tomato: Move boot animation out of device repo
    Change-Id: I494b8f41bf56bb8125c639e72112b3a8077f110b

--- cm.mk
+++ cm.mk
@@ -26,8 +26,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-micromax
 TARGET_VENDOR_PRODUCT_NAME := YUREKA
 TARGET_VENDOR_DEVICE_NAME := YUREKA
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=YUREKA PRODUCT_NAME=YUREKA
-
-ifeq ($(SIGN_BUILD),true)
-# Signed builds gets a special boot animation because they are special.
-PRODUCT_BOOTANIMATION := device/yu/tomato/bootanimation.zip
-endif
