f99180b691d62cabb52626fe8887a8bf6e7b6595
Ethan Chen <intervigil@gmail.com>
2015-03-25 21:23:04 -0700

    tomato: Add debug kernel snippet
    Change-Id: I258815ecc0d87c282f3d5af226625be0892e0445

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,9 @@ TARGET_USES_UNCOMPRESSED_KERNEL := true
 else
 TARGET_KERNEL_CONFIG := cyanogenmod_tomato_defconfig
 endif
+ifneq ($(TARGET_BUILD_VARIANT),user)
+TARGET_KERNEL_ADDITIONAL_CONFIG := cyanogenmod_debug_config
+endif
 
 # ANT+
 BOARD_ANT_WIRELESS_DEVICE := "vfs-prerelease"
