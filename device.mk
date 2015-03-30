4019edd520e7c2ddf5dd02ea453fb462972046a1
Steve Kondik <steve@cyngn.com>
2015-03-30 14:22:48 -0700

    tomato: Split out power config
     * So we can use it in recovery too.
    
    Change-Id: I4aa7c8e7d4b06aafa4f82117428bed4f65161b93

--- device.mk
+++ device.mk
@@ -217,6 +217,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
+    init.qcom.power.rc \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
     ueventd.qcom.rc
