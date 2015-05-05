f844242aaa750ac949de7623f6f0e892e20372e3
Ethan Chen <intervigil@gmail.com>
2015-05-04 18:35:57 -0700

    tomato: Move GPS HAL to device
    Change-Id: I9d60ca826132e1855adf4d0e604f91372ca6e07d

--- device.mk
+++ device.mk
@@ -59,6 +59,17 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libjni_cmhw
 
+# GPS
+PRODUCT_PACKAGES += \
+    gps.msm8916
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/gps/flp.conf:system/etc/flp.conf \
+    $(LOCAL_PATH)/gps/gps.conf:system/etc/gps.conf \
+    $(LOCAL_PATH)/gps/izat.conf:system/etc/izat.conf \
+    $(LOCAL_PATH)/gps/quipc.conf:system/etc/quipc.conf \
+    $(LOCAL_PATH)/gps/sap.conf:system/etc/sap.conf
+
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/goodix.kl:system/usr/keylayout/goodix.kl \
