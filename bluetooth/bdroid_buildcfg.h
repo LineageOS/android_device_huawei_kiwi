4b56bda0b225a1af26467ece116bdfee2e8f2263
Pat Erley <perley@cyngn.com>
2015-05-27 17:36:39 -0700

    Make Bluetooth Name Generic
    This makes the bluetooth discovery name non device specific.
    
    TOMATOL-514
    Change-Id: I54327cdac5a765c66cbf220f27c2d206bf591533

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "AO5510"
+#define BTM_DEF_LOCAL_NAME   "YUREKA"
 #define BLUETOOTH_QTI_SW TRUE
 // Disables read remote device feature
 #define BTA_SKIP_BLE_READ_REMOTE_FEAT FALSE
