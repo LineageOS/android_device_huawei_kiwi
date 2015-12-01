3d2c9f4d6cec1e6114de70895b03154b656e7093
Pat Erley <perley@cyngn.com>
2015-12-01 11:20:26 -0800

    tomato: enable subsystem restart on user builds
    CYNGNOS-1375
    
    Change-Id: I9aed6ad2ec0143c232ef5ff2df1a05788d56e685

--- device.mk
+++ device.mk
@@ -103,6 +103,12 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     init.target.rc
 
+# SSR
+ifeq ($(TARGET_BUILD_VARIANT),user)
+PRODUCT_PACKAGES += \
+    init.qcom.ssr.rc
+endif
+
 # Recovery
 PRODUCT_EXTRA_RECOVERY_KEYS += \
     $(LOCAL_PATH)/tomato-releasekey
