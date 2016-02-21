ddfbf78a4b6b432791d4ca9ae1fd6096829acd60
dianlujitao <dianlujitao@gmail.com>
2016-02-21 10:09:22 +0800

    kiwi: Rename device-specific healthd extension
     * Also remove deprecated BOARD_CHARGER_SHOW_PERCENTAGE
    
    Change-Id: I6f03fdb92a95a0cff0a1460a80267beb20da94e2

--- healthd/Android.mk
+++ healthd/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := healthd_board_msm.cpp
-LOCAL_MODULE := libhealthd.msm
+LOCAL_MODULE := libhealthd.$(TARGET_BOARD_PLATFORM)
 LOCAL_CFLAGS := -Werror
 LOCAL_C_INCLUDES := system/core/healthd bootable/recovery
 include $(BUILD_STATIC_LIBRARY)
