35c0f70c3931cfa49834d6816437d574ef37d2cc
Christopher R. Palmer <crpalmer@gmail.com>
2016-02-25 09:28:39 -0500

    kiwi: Add configuration files needed for the RIL
    Change-Id: Iaad0d7f6b9a2850b48f2101ac72a235f33f9b775

--- device.mk
+++ device.mk
@@ -174,6 +174,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libxml2
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dsi_config.xml:system/etc/data/dsi_config.xml \
+    $(LOCAL_PATH)/configs/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/qmi_config.xml:system/etc/data/qmi_config.xml
+
 # Sensors
 PRODUCT_PACKAGES += \
     libjni_proximityCalibrate \
