8b10ee48e42cabf83cf9ad1761749fe9fbebb949
Christopher R. Palmer <crpalmer@gmail.com>
2016-02-24 20:15:17 -0500

    kiwi: Add missing app_widgets permission
    Change-Id: I441c39b8767dcf2eb78bce2eb9ebf0c05bfd7d1e

--- device.mk
+++ device.mk
@@ -41,7 +41,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:system/etc/permissions/android.hardware.sensor.gyroscope.xml \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml
+    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/native/data/etc/android.software.app_widgets.xml:system/etc/permissions/android.software.app_widgets.xml
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
