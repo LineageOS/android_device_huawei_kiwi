697ec7eb4d2f72503d10046e7bc6b4b6856ca4ff
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-02-26 10:00:21 -0800

    Drop mkbootimg
    Use CMDTBTool
    
    Change-Id: I8b8746a43dd2c48f7f7e585bade75cdf197145c6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,11 +43,11 @@ TARGET_BOOTLOADER_BOARD_NAME := MSM8916
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_CUSTOM_BOOTIMG_MK := $(DEVICE_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
+BOARD_DTBTOOL_ARGS := -2
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
 TARGET_KERNEL_SOURCE := kernel/huawei/kiwi
