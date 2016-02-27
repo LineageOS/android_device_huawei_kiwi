8c82a1ff70151e6534bd1b3a09cdaf5eb52131ed
Christopher R. Palmer <crpalmer@gmail.com>
2016-02-27 07:05:43 -0500

    kiwi: audio: Initial bringup
    Change-Id: I6b5d4a0cf8f8cb8f8b0ec3a40adaa20c848ac1d2

--- device.mk
+++ device.mk
@@ -73,7 +73,10 @@ PRODUCT_PACKAGES += \
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_skuk.xml
 
 # ANT+
 PRODUCT_PACKAGES += \
