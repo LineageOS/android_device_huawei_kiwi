2f3703578f0bfb9bbd3a31891122aa8708ffaa7c
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-03-03 16:59:54 -0800

    Copy over configs
    Change-Id: I81d53d0d62404b251836235e625f6140dc94beab

--- device.mk
+++ device.mk
@@ -1,4 +1,4 @@
-#
+e#
 # Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -218,6 +218,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/lowi.conf:system/etc/lowi.conf
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
