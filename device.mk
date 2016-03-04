1ca91f93ecbf665a2340c65269ba15a93ac2f007
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-03-03 16:59:54 -0800

    Copy over configs
    Change-Id: I81d53d0d62404b251836235e625f6140dc94beab

--- device.mk
+++ device.mk
@@ -220,6 +220,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/lowi.conf:system/etc/lowi.conf
+
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_wlan_dictionary.dat:system/etc/wifi/WCNSS_wlan_dictionary.dat \
