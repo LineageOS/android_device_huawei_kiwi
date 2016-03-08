ef5c9fa27045012ea25c64677714c7662380591d
Christopher R. Palmer <crpalmer@gmail.com>
2016-03-08 06:10:48 -0500

    kiwi: proprietary: Allow extract-files to pull from 2 sources
    The normal usage would be:
    
    ./extract-files $OUT $OUT/root $STOCK $STOCK/root
    
    to pull everything listed from your current build ($OUT) and if
    doesn't exist from your unpackaged stock ROM ($STOCK).
    
    This makes it easy to add a pile of files and then pull them in.
    
    Change-Id: I288efc466bc76b2365d8d527df8d00e2956b2ca4

--- extract-files.sh
+++ extract-files.sh
@@ -10,10 +10,17 @@ else
   if [ $# -eq 2 ]; then
     SRC=$1
     RAMDISK=$2
+    SRC2=$1
+    RAMDISK2=$2
+  elif [ $# -eq 4 ]; then
+    SRC=$1
+    RAMDISK=$2
+    SRC2=$3
+    RAMDISK2=$4
   else
     echo "$0: bad number of arguments"
     echo ""
-    echo "usage: $0 [PATH_TO_EXPANDED_ROM RAMDISK_TO_EXPAND_FROM]"
+    echo "usage: $0 [PATH_TO_EXPANDED_ROM RAMDISK_TO_EXPAND_FROM [ALT_EXPANDED ALT_RAMDISK]]"
     echo ""
     echo "If PATH_TO_EXPANDED_ROM is not specified, blobs will be extracted from"
     echo "the device using adb pull."
@@ -35,9 +42,11 @@ for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
   if [ "${FILE:0:6}" = "/sbin/" ]; then
     DIR_PREFIX=""
     LOCAL_DIR=$RAMDISK
+    LOCAL_DIR2=$RAMDISK2
   else
     DIR_PREFIX=/system/
     LOCAL_DIR=$SRC
+    LOCAL_DIR2=$SRC2
   fi
   DIR=`dirname $DEST`
   if [ ! -d $BASE/$DIR ]; then
@@ -57,8 +66,15 @@ for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
   else
     if [ -r $LOCAL_DIR/$DIR_PREFIX/$DEST ]; then
         cp $LOCAL_DIR/$DIR_PREFIX/$DEST $BASE/$DEST
-    else
+    elif [ -r $LOCAL_DIR/$DIR_PREFIX/$FILE ]; then
         cp $LOCAL_DIR/$DIR_PREFIX/$FILE $BASE/$DEST
+    elif [ -r $LOCAL_DIR2/$DIR_PREFIX/$DEST ]; then
+        cp $LOCAL_DIR2/$DIR_PREFIX/$DEST $BASE/$DEST
+    elif [ -r $LOCAL_DIR2/$DIR_PREFIX/$FILE ]; then
+        cp $LOCAL_DIR2/$DIR_PREFIX/$FILE $BASE/$DEST
+    else
+        echo "error: missing file $FILE:$DEST"
+        exit 1
     fi
   fi
 done
