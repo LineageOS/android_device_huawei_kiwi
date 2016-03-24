f54c1194abeba1ead749932048be3b2a12451034
Christopher R. Palmer <crpalmer@gmail.com>
2016-03-24 12:22:12 -0400

    kiwi: Use AOSP FMRadio package
    Change-Id: Icd350377fd84ee4511934acbdabac4ea2b9d68ab

--- device.mk
+++ device.mk
@@ -122,10 +122,8 @@ PRODUCT_PACKAGES += \
 
 # FM
 PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+    FMRadio \
+    libfmjni
 
 # GPS
 PRODUCT_PACKAGES += \
