cd6c4403e58e991d6b5b223fd2abb36cce55001a
Christopher R. Palmer <crpalmer@gmail.com>
2016-05-02 06:02:11 -0400

    kiwi: power: Kill cpu boost / touch boost
    Change-Id: I0fcefc03b23117470c67ea0b56f5fab9d831e6d2

--- power/power-kiwi.c
+++ power/power-kiwi.c
@@ -127,42 +127,6 @@ int power_hint_override(struct power_module *module __unused, power_hint_t hint,
     }
 
     if (hint == POWER_HINT_INTERACTION) {
-        int duration = 0;
-        static unsigned long long previous_boost_time = 0;
-
-        if (data) {
-            duration = *((int *)data);
-        }
-
-        if (duration == 0) {
-            duration = DEFAULT_INTERACTION_BOOST_MS;
-        }
-
-        struct timeval cur_boost_timeval = {0, 0};
-        gettimeofday(&cur_boost_timeval, NULL);
-        unsigned long long cur_boost_time = cur_boost_timeval.tv_sec * 1000000 + cur_boost_timeval.tv_usec;
-        double elapsed_time = (double)(cur_boost_time - previous_boost_time);
-        if (elapsed_time > 750000)
-            elapsed_time = 750000;
-        // don't hint if it's been less than 250ms since last boost
-        // also detect if we're doing anything resembling a fling
-        // support additional boosting in case of flings
-        else if (elapsed_time < 250000 && duration <= 750)
-            return HINT_HANDLED;
-
-        previous_boost_time = cur_boost_time;
-
-        if (duration > MAX_INTERACTION_BOOST_MS) {
-            duration = MAX_INTERACTION_BOOST_MS;
-        }
-
-        if (duration >= 1500) {
-            int resources[] = { SCHED_BOOST_ON, 0x20D, 0x101, 0x3E01 };
-            interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
-        } else {
-            int resources[] = { 0x20D, 0x101, 0x3E01 };
-            interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
-        }
         return HINT_HANDLED;
     }
 
@@ -176,16 +140,6 @@ int power_hint_override(struct power_module *module __unused, power_hint_t hint,
     }
 
     if (hint == POWER_HINT_CPU_BOOST) {
-        int duration = *(int32_t *)data / 1000;
-        int resources[] = { SCHED_BOOST_ON, 0x20D, 0x3E01, 0x101 };
-
-        if (duration > 0) {
-            if (duration > MAX_CPU_BOOST_MS) {
-                duration = MAX_CPU_BOOST_MS;
-            }
-            interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
-        }
-
         return HINT_HANDLED;
 	}
 
