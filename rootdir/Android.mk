bdc9ffa2e5462eb97ebe3695775574a43fca41aa
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-06-04 16:47:39 -0700

    WIP Quick and dirty (very dirty) sepolicy
    Change-Id: I0fd4f970f2f2a9ea6b2c11a3ac1df6562720cbb3

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -44,9 +44,9 @@ include $(BUILD_PREBUILT)
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.power.sh
 LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_CLASS := EXECUTABLES
 LOCAL_SRC_FILES    := etc/init.qcom.power.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
