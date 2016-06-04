a6a0e21c410c19d75a7ea7f818948d65d194d955
Abhisek Devkota <ciwrl@cyanogenmod.com>
2016-06-04 16:47:39 -0700

    WIP Quick and dirty (very dirty) sepolicy
    Change-Id: I0fd4f970f2f2a9ea6b2c11a3ac1df6562720cbb3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -153,6 +153,9 @@ TARGET_RIL_VARIANT := proprietary
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 
+BOARD_SEPOLICY_DIRS += \
+    device/huawei/kiwi/sepolicy
+
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_kiwi
