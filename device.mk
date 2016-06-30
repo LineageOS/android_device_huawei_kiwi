bb929c96b38e52830af15f6233acd2ad743cf338
Rudolf Tammekivi <rtammekivi@gmail.com>
2016-06-30 18:22:00 +0200

    kiwi: Include libwcnss_qmi into the build
    Change-Id: Ifc0fed1dac801d410cd990d01f6a901ba258d64b

--- device.mk
+++ device.mk
@@ -280,7 +280,8 @@ PRODUCT_PACKAGES += \
     hostapd \
     wpa_supplicant \
     wpa_supplicant.conf \
-    wcnss_service
+    wcnss_service \
+    libwcnss_qmi
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
