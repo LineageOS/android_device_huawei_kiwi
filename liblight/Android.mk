44710bfcc63e5f4b8c878eec07d7420f5ed048ec
Rudolf Tammekivi <rtammekivi@gmail.com>
2016-07-01 01:16:34 +0200

    kiwi: Add support for modem side LED notifications
    Huawei has a special API in modem that allows having notification LEDs without
    real hardware support.
    
    The oem_qmi_common_stream_from_modem_len function call and arguments are reverse
    engineered from the stock lights HAL module.
    
    Change-Id: I5b084fcafbe525e079d9e188ae823e299cb19b5c

--- liblight/Android.mk
+++ liblight/Android.mk
@@ -19,8 +19,7 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := lights.c
 LOCAL_MODULE_RELATIVE_PATH    := hw
-LOCAL_SHARED_LIBRARIES := liblog libcutils
-
+LOCAL_SHARED_LIBRARIES := liblog libcutils libqmi_oem_api
 LOCAL_MODULE := lights.msm8916
 LOCAL_MODULE_TAGS := optional
 
--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -91,6 +91,9 @@ PRODUCT_PACKAGES += \\
 PRODUCT_PACKAGES += \\
     libtfa9895
 
+PRODUCT_PACKAGES += \\
+    libqmi_oem_api
+
 \$(call inherit-product, vendor/$VENDOR/$DEVICE/$DEVICE-vendor-blobs.mk)
 EOF
 
@@ -263,6 +266,19 @@ include \$(BUILD_PREBUILT)
 
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
 \$(shell mkdir -p \$(PRODUCT_OUT)/system/vendor/lib64/egl && pushd \$(PRODUCT_OUT)/system/vendor/lib64 > /dev/null && ln -s egl/libEGL_adreno.so libEGL_adreno.so && popd > /dev/null)
+
+include \$(CLEAR_VARS)
+LOCAL_MODULE := libqmi_oem_api
+LOCAL_MODULE_OWNER := $VENDOR
+LOCAL_SRC_FILES_64 := proprietary/lib64/libqmi_oem_api.so
+LOCAL_SRC_FILES_32 := proprietary/lib/libqmi_oem_api.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH_64 := \$(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE_PATH_32 := \$(2ND_TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MULTILIB := both
+include \$(BUILD_PREBUILT)
 
 endif
 endif
