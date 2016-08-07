a84a66e91c905d3a20a1dc286443fb4d9a06ea77
TheScarastic <warabhishek@gmail.com>
2016-08-07 11:34:29 -0400

    kiwi: Clean commandline
     * remove earlyprintk causes longer boot
     * remove msm_rtb disabled kernel side
    
    Change-Id: Ic2057dc423247309e1c4b342d5e543a10e8c4fc5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ TARGET_OTA_ASSERT_DEVICE := kiwi
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
