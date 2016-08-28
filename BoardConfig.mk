642b0d0f9cdc10dbfae0e7ecd1d504bc54663980
Michael W <baddaemon87@gmail.com>
2016-08-28 14:45:32 +0200

    kiwi: Switch to CM healthd
    * Adds newer charging pictures
    * Replaces device specific healthd-extension with a generic one
      (Found in vendor/cm/charger)
    
    Change-Id: I366820f5845697cdcc8be7c8257d25d0d4abefd0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -78,7 +78,7 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 
 # Charger
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
-BOARD_HAL_STATIC_LIBRARIES += libhealthd.msm8916
+BACKLIGHT_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # CM Hardware
 BOARD_HARDWARE_CLASS += \
