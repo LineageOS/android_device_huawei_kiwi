64fd753e2219be1ab6c45509bc9b91dab37d67e3
Michael W <baddaemon87@gmail.com>
2016-09-11 14:14:51 +0200

    kiwi: ril: Scale gsmSignalStrength properly
    Seems like the parcel already contains dBm values,
    but asu is expected. Apply reverse calculation so the
    value is in a valid range (0..31) and we get a proper signal bar.
    (Currently we have FULL and NONE)
    Issue can be reproduced by forcing 2G network.
    
    Change-Id: I671738813bb62701dfc930e26104e25afcf7ce75

--- ril/telephony/java/com/android/internal/telephony/KiwiRIL.java
+++ ril/telephony/java/com/android/internal/telephony/KiwiRIL.java
@@ -33,6 +33,8 @@ import android.telephony.SignalStrength;
  * {@hide}
  */
 public class KiwiRIL extends RIL {
+    private final boolean DBG = false;
+
     public KiwiRIL(Context context, int networkMode, int cdmaSubscription, Integer instanceId) {
         super(context, networkMode, cdmaSubscription, instanceId);
     }
@@ -86,12 +88,22 @@ public class KiwiRIL extends RIL {
         int lteRssnr = p.readInt();
         int lteCqi = p.readInt();
 
-        return new SignalStrength(
+        if (gsmSignalStrength != -1) {
+            gsmSignalStrength = -(gsmSignalStrength - 113) / 2;
+        }
+
+        SignalStrength ss = new SignalStrength(
                         wcdmaRscp <= 0 ? gsmSignalStrength : wcdmaRscp,
                         gsmBitErrorRate,
                         cdmaDbm, cdmaEcio,
                         evdoDbm, evdoEcio, evdoSnr,
                         lteSignalStrength, lteRsrp, lteRsrq, lteRssnr, lteCqi,
                         wcdmaRscp, true);
+
+        if (DBG) {
+            Rlog.i(RILJ_LOG_TAG, "KiwiRil: " + ss.toString() + " " + gsmSignalStrength);
+        }
+
+        return ss;
     }
 }
