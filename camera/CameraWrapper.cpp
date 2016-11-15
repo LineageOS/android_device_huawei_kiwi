10df2e087fe692425e5ceba3626cdcb9d17790e2
LuK1337 <priv.luk@gmail.com>
2016-11-15 14:40:59 +0100

    kiwi: camera: Disable longshot
    * Doesn't work, maybe for the same case HDR is not working
    
    Change-Id: I1d727bcba32dabb5cf7e3dc3ce1e62265b66c384

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -117,6 +117,7 @@ static char *camera_fixup_getparams(int id, const char *settings)
 #endif
 
     params.set("scene-mode-values", "auto");
+    params.set("longshot-supported", "false");
 
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
