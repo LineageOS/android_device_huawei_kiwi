fdd0bcc988fd11d5749db68b6ba40c8dcc693090
aphisitworachorch <thaixperiasp@gmail.com>
2017-01-03 02:23:35 +0100

    kiwi: Doze: Rename OnePlus occurances
    * We are kiwi
    
    Change-Id: I5d53ce8d176f2e8952cef7ca6bfe9752839d6af9

--- doze/Android.mk
+++ doze/Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := OneplusDoze
+LOCAL_PACKAGE_NAME := KiwiDoze
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVILEGED_MODULE := true
 LOCAL_STATIC_JAVA_LIBRARIES := \
