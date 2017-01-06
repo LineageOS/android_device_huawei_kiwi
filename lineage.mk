a029fcff5dfc593ba103f86c67d57bd8ba2fd1c9
Michael W <baddaemon87@gmail.com>
2017-01-06 15:09:20 +0100

    kiwi : Rebrand To LineageOS
    Change-Id: I4a2e9a102590b4782ea2cbd0c31db63b2f893522
    Signed-off-by: aphisitworachorch <thaixperiasp@gmail.com>

--- lineage.mk
+++ lineage.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_kiwi
+PRODUCT_NAME := lineage_kiwi
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
