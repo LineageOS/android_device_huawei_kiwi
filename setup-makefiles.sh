e0a3d997fd6acbfba328f30183ac3a6bdc5f99d2
Michael W <baddaemon87@gmail.com>
2017-01-08 00:42:31 +0100

    kiwi: Fix files
    * Remove libloc_ds_api - never made it in
    * Remove com.qualcomm.location (remainder from tomato kang)
    
    Change-Id: I95d82ce86bc1d5494427c21eb488099436a4033f

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -115,6 +115,7 @@ EOF
 
 (cat << EOF) > ../../../$OUTDIR/Android.mk
 # Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -134,17 +135,6 @@ LOCAL_PATH := \$(call my-dir)
 
 ifeq (\$(TARGET_DEVICE),kiwi)
 ifeq (\$(QCPATH),)
-
-include \$(CLEAR_VARS)
-LOCAL_MODULE := com.qualcomm.location
-LOCAL_MODULE_OWNER := $VENDOR
-LOCAL_SRC_FILES := proprietary/priv-app/com.qualcomm.location/com.qualcomm.location.apk
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_SUFFIX := \$(COMMON_ANDROID_PACKAGE_SUFFIX)
-LOCAL_MODULE_CLASS := APPS
-LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
-include \$(BUILD_PREBUILT)
 
 include \$(CLEAR_VARS)
 LOCAL_MODULE := qcrilmsgtunnel
