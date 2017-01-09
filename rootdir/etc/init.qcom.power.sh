ca830ddd85e9cf7bc614209473bf8982a2d93690
Christopher R. Palmer <crpalmer@gmail.com>
2017-01-09 10:49:01 -0500

    kiwi: Remove extra checks in init.qcom.power.sh
    This is our script for our device, just run the commands.  No need to
    verify that we have the right SOC.
    
    Change-Id: Ie20ac351234c84e921c4840ad348dad0fe6d7baf

--- rootdir/etc/init.qcom.power.sh
+++ rootdir/etc/init.qcom.power.sh
@@ -14,35 +14,23 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-export PATH=/system/bin
-
-if [ -f /sys/devices/soc0/soc_id ]; then
-    soc_hwid=`cat /sys/devices/soc0/soc_id`
-else
-    soc_hwid=`cat /sys/devices/system/soc/soc0/id`
-fi
-
 # Set devfreq parameters for MSM8939
-case "$soc_hwid" in
-    233|239|240|241|242|243|263|268|269|270|271)
-        for devfreq_gov in /sys/class/devfreq/qcom,mincpubw*/governor
-        do
-            echo "cpufreq" > $devfreq_gov
-        done
+for devfreq_gov in /sys/class/devfreq/qcom,mincpubw*/governor
+do
+    echo "cpufreq" > $devfreq_gov
+done
 
-        for devfreq_gov in /sys/class/devfreq/qcom,cpubw*/governor
-        do
-            echo "bw_hwmon" > $devfreq_gov
-        done
+for devfreq_gov in /sys/class/devfreq/qcom,cpubw*/governor
+do
+    echo "bw_hwmon" > $devfreq_gov
+done
 
-        for cpu_io_percent in /sys/class/devfreq/qcom,cpubw*/bw_hwmon/io_percent
-        do
-            echo 20 > $cpu_io_percent
-        done
+for cpu_io_percent in /sys/class/devfreq/qcom,cpubw*/bw_hwmon/io_percent
+do
+    echo 20 > $cpu_io_percent
+done
 
-        for gpu_bimc_io_percent in /sys/class/devfreq/qcom,gpubw*/bw_hwmon/io_percent
-        do
-            echo 40 > $gpu_bimc_io_percent
-        done
-        ;;
-esac
+for gpu_bimc_io_percent in /sys/class/devfreq/qcom,gpubw*/bw_hwmon/io_percent
+do
+    echo 40 > $gpu_bimc_io_percent
+done
