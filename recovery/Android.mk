cb2c678c179ea657137e7d4ffe4a13ab64de4d8c
Michael Bestas <mkbestas@lineageos.org>
2017-04-28 22:34:12 +0300

    kiwi: recovery: Use project pathmap for recovery
    Change-Id: Ifb7176454b5237ec366e5634e0188a6ce24740dd

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,7 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_C_INCLUDES := $(call project-path-for,recovery)
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_kiwi
 LOCAL_MODULE_TAGS := eng
