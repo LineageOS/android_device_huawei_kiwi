8792de122046246b4c6ca3064e7b0c214040cbdc
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:15:58 +0200

    kiwi: Remove unused libqsap_sdk & libQWiFiSoftApCfg
    Change-Id: Ib543425786c498eec8ef421f85047bcb1cb0cae3

--- device.mk
+++ device.mk
@@ -267,8 +267,6 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    libqsap_sdk \
-    libQWiFiSoftApCfg \
     libwpa_client \
     wificond \
     hostapd \
