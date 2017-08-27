193f8ab90b5688f8a319f7133fd92e39e852abcc
Luca Stefani <luca020400@lineageos.org>
2017-08-27 20:32:05 +0200

    kiwi: Kill unused hostapd configs
    Change-Id: I9eb3775be108b334c9b74306a37898ddec6fa9ec

--- device.mk
+++ device.mk
@@ -263,9 +263,6 @@ PRODUCT_PACKAGES += \
     wcnss_service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/hostapd.accept:system/etc/hostapd/hostapd.accept \
-    $(LOCAL_PATH)/configs/hostapd.conf:system/etc/hostapd/hostapd_default.conf \
-    $(LOCAL_PATH)/configs/hostapd.deny:system/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/vendor/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/vendor/etc/wifi/wpa_supplicant_overlay.conf
 
