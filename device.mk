478809efc0d5be0e00c1a99b3c1224461588b648
nicknitewolf <nicknitewolf@lineageos.org>
2017-09-06 20:54:48 +0800

    kiwi: Use lineage custom legacy camera provider
    * Camera HALs before Treble used to pass camera id
      in user field, in Treble they are meant to pass
      pointer to CameraDevice.
    * This patch workarounds this by storing local pointer
      to CameraDevice and not relying on user field.
    
    Change-Id: I6a26f8f9a3870d4b2aefad86307da036a8f1996c

--- device.mk
+++ device.mk
@@ -91,8 +91,8 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl \
+    android.hardware.camera.provider@2.4-impl-legacy \
+    camera.device@1.0-impl-legacy \
     camera.msm8916 \
     libmm-qcamera \
     Snap
