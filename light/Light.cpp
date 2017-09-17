59ba9abfa07c192ed1860d4960cce25820b53f90
Ethan Chen <intervigil@gmail.com>
2017-09-17 11:27:00 +0200

    kiwi: Convert lights HAL to native binderized HAL
    Change-Id: I43673573af05d1118cbcd3d0146749f143745a84

---
+++ light/Light.cpp
@@ -0,0 +1,163 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "light"
+
+#include "Light.h"
+
+#include <log/log.h>
+
+namespace {
+using android::hardware::light::V2_0::LightState;
+
+static uint32_t rgbToBrightness(const LightState& state) {
+    uint32_t color = state.color & 0x00ffffff;
+    return ((77 * ((color >> 16) & 0xff)) + (150 * ((color >> 8) & 0xff)) +
+            (29 * (color & 0xff))) >> 8;
+}
+
+static bool isLit(const LightState& state) {
+    return (state.color & 0x00ffffff);
+}
+
+/* libqmi_oem_api.so */
+struct qmi_huawei_not {
+    unsigned int color;
+    int flashOnMs;
+    int flashOffMs;
+    int flashMode;
+} __attribute__((packed));
+
+static constexpr int QMI_HUAWEI_NOT_ID = 0x8F;
+extern "C" int oem_qmi_common_stream_from_modem_len(int id,
+                                                    void *buf_in,
+                                                    size_t buf_in_size,
+                                                    void *buf_out,
+                                                    size_t *buf_out_size);
+} // anonymous namespace
+
+namespace android {
+namespace hardware {
+namespace light {
+namespace V2_0 {
+namespace implementation {
+
+Light::Light(std::ofstream&& backlight) :
+    mBacklight(std::move(backlight)) {
+    auto attnFn(std::bind(&Light::setAttentionLight, this, std::placeholders::_1));
+    auto backlightFn(std::bind(&Light::setBacklight, this, std::placeholders::_1));
+    auto batteryFn(std::bind(&Light::setBatteryLight, this, std::placeholders::_1));
+    auto notifFn(std::bind(&Light::setNotificationLight, this, std::placeholders::_1));
+    mLights.emplace(std::make_pair(Type::ATTENTION, attnFn));
+    mLights.emplace(std::make_pair(Type::BACKLIGHT, backlightFn));
+    mLights.emplace(std::make_pair(Type::BATTERY, batteryFn));
+    mLights.emplace(std::make_pair(Type::NOTIFICATIONS, notifFn));
+}
+
+// Methods from ::android::hardware::light::V2_0::ILight follow.
+Return<Status> Light::setLight(Type type, const LightState& state) {
+    if (mLights.find(type) != mLights.end()) {
+        mLights.at(type)(state);
+        return Status::SUCCESS;
+    }
+    return Status::LIGHT_NOT_SUPPORTED;
+}
+
+Return<void> Light::getSupportedTypes(getSupportedTypes_cb _hidl_cb) {
+    Type *types = new Type[mLights.size()];
+    int idx = 0;
+
+    for (auto const &kv : mLights) {
+        Type t = kv.first;
+        types[idx++] = t;
+    }
+
+    {
+        hidl_vec<Type> hidl_types{};
+        hidl_types.setToExternal(types, mLights.size());
+
+        _hidl_cb(hidl_types);
+    }
+
+    delete[] types;
+
+    return Void();
+}
+
+void Light::setAttentionLight(const LightState& state) {
+    std::lock_guard<std::mutex> lock(mLock);
+    mAttentionState = state;
+    setSpeakerBatteryLightLocked();
+}
+
+void Light::setBacklight(const LightState& state) {
+    std::lock_guard<std::mutex> lock(mLock);
+
+    uint32_t brightness = rgbToBrightness(state);
+
+    mBacklight << brightness << std::endl;
+}
+
+void Light::setBatteryLight(const LightState& state) {
+    std::lock_guard<std::mutex> lock(mLock);
+    mBatteryState = state;
+    setSpeakerBatteryLightLocked();
+}
+
+void Light::setNotificationLight(const LightState& state) {
+    std::lock_guard<std::mutex> lock(mLock);
+    mNotificationState = state;
+    setSpeakerBatteryLightLocked();
+}
+
+void Light::setSpeakerBatteryLightLocked() {
+    if (isLit(mNotificationState)) {
+        setSpeakerLightLocked(mNotificationState);
+    } else if (isLit(mAttentionState)) {
+        setSpeakerLightLocked(mAttentionState);
+    } else {
+        setSpeakerLightLocked(mBatteryState);
+    }
+}
+
+void Light::setSpeakerLightLocked(const LightState& state) {
+    int ret = 0;
+    char buf = 0;
+    size_t bufSize = 0;
+    qmi_huawei_not config = { .color = state.color,
+                              .flashOnMs = 0,
+                              .flashOffMs = 0,
+                              .flashMode = static_cast<int>(state.flashMode) };
+
+    if (state.flashMode == Flash::TIMED) {
+        config.flashOnMs = state.flashOnMs;
+        config.flashOffMs = state.flashOffMs;
+    }
+
+    ret = oem_qmi_common_stream_from_modem_len(QMI_HUAWEI_NOT_ID, &config,
+                                               sizeof(config), &buf, &bufSize);
+
+    if (ret != 1) {
+        ALOGE("Failed to write notification LED ret=%d, buf_out_size=%zu\n",
+              ret, bufSize);
+    }
+}
+
+} // namespace implementation
+}  // namespace V2_0
+}  // namespace light
+}  // namespace hardware
+}  // namespace android
