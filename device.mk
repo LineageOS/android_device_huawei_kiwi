7310a017bf843d9c0c92079028fe0400e8520d20
Michael W <baddaemon87@gmail.com>
2017-10-15 16:33:37 +0200

    kiwi: Use correct screen width
    * Display the boot animation the way the creator intended it
    
    Change-Id: I2c58eec679383b88a759c103a7ff7699abb6ef75

--- device.mk
+++ device.mk
@@ -51,7 +51,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1280
+TARGET_SCREEN_WIDTH := 1080
 
 $(call inherit-product, frameworks/native/build/phone-hdpi-2048-dalvik-heap.mk)
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
