e20379c385abe351d24563f5bdd8c7eb3f33cf87
Michael W <baddaemon87@gmail.com>
2017-10-28 14:46:15 +0200

    kiwi: Adapt to new bootimage name
    * Commit edc1cebc2f7baedf6479874db2848b45e22f4205 introduced a new way
      to name bootimages
    * Ours is arm64, uncompressed and separated DT, so the name is just "Image"
    
    Change-Id: I7a7df99d327b516d2fe7a29a5b7907acea045c67

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -42,6 +42,7 @@ TARGET_OTA_ASSERT_DEVICE := kiwi
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 earlyprintk androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_DTBTOOL_ARGS := -2
@@ -51,7 +52,6 @@ TARGET_KERNEL_SOURCE := kernel/huawei/kiwi
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_USES_UNCOMPRESSED_KERNEL := true
 TARGET_KERNEL_CONFIG := kiwi-64_defconfig
 
 # Audio
