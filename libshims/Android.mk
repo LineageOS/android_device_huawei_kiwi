62b34c40f0b65918c30b0d03cc92b616ace01239
Dobroslaw Kijowski <dobo90@gmail.com>
2017-12-03 22:27:29 +0100

    Revert "kiwi: shims: camera shim"
    This reverts commit 671dfb9113804157466ec166bdf17ba0951de0de.
    
    Change-Id: Ie03badbee4cdb364c7d1aeadcec37e33ed9f9ca4

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -52,16 +52,3 @@ LOCAL_SRC_FILES := hw_audio.c
 LOCAL_MODULE := libshim_sound_trigger
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
-
-# Camera
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := \
-    gui/SensorManager.cpp \
-    ui/GraphicBufferAllocator.cpp \
-    ui/GraphicBuffer.cpp \
-    ui/GraphicBufferMapper.cpp \
-    surface-control.cpp
-LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware libui libgui libbinder libutils libsync
-LOCAL_MODULE := libshim_camera
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-include $(BUILD_SHARED_LIBRARY)
