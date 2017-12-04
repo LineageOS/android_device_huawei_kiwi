321e1f99fb64c4cf04394e09116127dac9dcba37
LuK1337 <priv.luk@gmail.com>
2017-12-04 19:29:50 +0100

    kiwi: Include legacy QCOM sepolicy
    Change-Id: I58ab97157b850731a5a1537a80a2bf3d7d3a5126

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -157,6 +157,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/legacy-sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/huawei/kiwi/sepolicy
