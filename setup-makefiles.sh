f5597f6236d3423dc7e4437fff684815a0e5fe43
Michael W <baddaemon87@gmail.com>
2017-12-06 01:44:35 +0100

    kiwi: camera: Adjust setup-makefiles.sh
    * To reflect the current change to oss-hal and the removal of the
      blobs, we need to adjust the copyright as well
    
    Change-Id: I9a96232292554ed2bf35a1bd6b5131349f408480

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -8,6 +8,7 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 
 (cat << EOF) > $MAKEFILE
 # Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
