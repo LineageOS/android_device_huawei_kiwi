614057ed201d4694561f07ef4c94d76346c10382
Dobroslaw Kijowski <dobo90@gmail.com>
2018-01-18 20:35:19 +0100

    kiwi: remove superfluous shims
    Delete following modules due to:
    * audio.primary.msm8916 and libril are built from source
    * liblocationservice isn't provided anymore
    * libshim_camera_post_mediaserver and libshim_signinfolistener are needed only in 32-bits versions
    * libboringssl-compat and libstlport not needed anymore
    
    Change-Id: I13013621de7a5f4f160493163b34d375613536ca

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -18,6 +18,7 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := hw_exif.c
 LOCAL_MODULE := libshim_camera_post_mediaserver
 LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -27,28 +28,9 @@ LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := hw_gps.c
-LOCAL_MODULE := libshim_gps
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := ril.c
-LOCAL_SHARED_LIBRARIES := libbinder
-LOCAL_MODULE := libshim_ril
-LOCAL_MODULE_TAGS := optional
-LOCAL_MULTILIB := 64
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := icu55.c
 LOCAL_SHARED_LIBRARIES := libicuuc libicui18n
 LOCAL_MODULE := libshim_signinfolistener
 LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := hw_audio.c
-LOCAL_MODULE := libshim_sound_trigger
-LOCAL_MODULE_TAGS := optional
+LOCAL_MULTILIB := 32
 include $(BUILD_SHARED_LIBRARY)
