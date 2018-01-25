e6d26ea098439f2f5060097ca1b7bc4a3533f50e
Michael Bestas <mkbestas@lineageos.org>
2018-01-25 23:13:40 +0200

    kiwi: Drop audiod
    * It's now deprecated
    
    Change-Id: I00c8bdc09da4ba39508314ec8feba67d60e4fb04

--- device.mk
+++ device.mk
@@ -61,7 +61,6 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    audiod \
     audio.a2dp.default \
     audio.primary.msm8916 \
     audio.r_submix.default \
