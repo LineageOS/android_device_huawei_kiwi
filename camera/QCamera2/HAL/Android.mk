1eeabf8c95584681a44bdabbc41623dc7eafe181
Michael Bestas <mkbestas@lineageos.org>
2018-03-19 04:26:16 +0200

    QCamera2: Cleanup makefiles
    Change-Id: I96bd8a79e74e6bed8bcc8d39010411de26ba0fcd

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -2,29 +2,29 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_CLANG_CFLAGS += \
-        -Wno-error=unused-private-field \
-        -Wno-error=strlcpy-strlcat-size \
-        -Wno-error=gnu-designator \
-        -Wno-error=unused-variable \
-        -Wno-error=format
+    -Wno-error=unused-private-field \
+    -Wno-error=strlcpy-strlcat-size \
+    -Wno-error=gnu-designator \
+    -Wno-error=unused-variable \
+    -Wno-error=format
 
 LOCAL_SRC_FILES := \
-        QCamera2Factory.cpp \
-        QCamera2Hal.cpp \
-        QCamera2HWI.cpp \
-        QCameraMem.cpp \
-        ../util/QCameraQueue.cpp \
-        ../util/QCameraCmdThread.cpp \
-        QCameraStateMachine.cpp \
-        QCameraChannel.cpp \
-        QCameraStream.cpp \
-        QCameraPostProc.cpp \
-        QCamera2HWICallbacks.cpp \
-        QCameraParameters.cpp \
-        QCameraThermalAdapter.cpp \
-        wrapper/QualcommCamera.cpp
+    QCamera2Factory.cpp \
+    QCamera2Hal.cpp \
+    QCamera2HWI.cpp \
+    QCameraMem.cpp \
+    ../util/QCameraQueue.cpp \
+    ../util/QCameraCmdThread.cpp \
+    QCameraStateMachine.cpp \
+    QCameraChannel.cpp \
+    QCameraStream.cpp \
+    QCameraPostProc.cpp \
+    QCamera2HWICallbacks.cpp \
+    QCameraParameters.cpp \
+    QCameraThermalAdapter.cpp \
+    wrapper/QualcommCamera.cpp
 
-LOCAL_CFLAGS = -Wall -Wextra -Werror
+LOCAL_CFLAGS += -Wall -Wextra -Werror
 LOCAL_CFLAGS += -DHAS_MULTIMEDIA_HINTS
 
 #use media extension
@@ -40,22 +40,19 @@ LOCAL_CFLAGS += -DUSE_MEDIA_EXTENSIONS
 #endif
 
 LOCAL_C_INCLUDES := \
-        $(LOCAL_PATH)/../stack/common \
-        frameworks/native/include/media/openmax \
-        $(call project-path-for,qcom-display)/libgralloc \
-        $(call project-path-for,qcom-display)/libqdutils \
-        $(call project-path-for,qcom-media)/libstagefrighthw \
-        system/media/camera/include \
-        $(LOCAL_PATH)/../../mm-image-codec/qexif \
-        $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
-        $(LOCAL_PATH)/../util \
-        $(LOCAL_PATH)/wrapper \
-        system/media/camera/include
-
-LOCAL_C_INCLUDES += \
-        $(TARGET_OUT_HEADERS)/qcom/display
-LOCAL_C_INCLUDES += \
-        $(call project-path-for,qcom-display)/libqservice
+    $(LOCAL_PATH)/../stack/common \
+    frameworks/native/include/media/openmax \
+    $(call project-path-for,qcom-display)/libgralloc \
+    $(call project-path-for,qcom-display)/libqdutils \
+    $(call project-path-for,qcom-display)/libqservice \
+    $(call project-path-for,qcom-media)/libstagefrighthw \
+    system/media/camera/include \
+    $(LOCAL_PATH)/../../mm-image-codec/qexif \
+    $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
+    $(LOCAL_PATH)/../util \
+    $(LOCAL_PATH)/wrapper \
+    system/media/camera/include \
+    $(TARGET_OUT_HEADERS)/qcom/display
 
 LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
 LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
@@ -73,7 +70,7 @@ LOCAL_SHARED_LIBRARIES += libts_face_beautify_hal libts_detected_face_hal
 endif
 LOCAL_SHARED_LIBRARIES += libqdMetaData libqservice libbinder
 
-LOCAL_MODULE_RELATIVE_PATH    := hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_32_BIT_ONLY := true
 LOCAL_MODULE_TAGS := optional
