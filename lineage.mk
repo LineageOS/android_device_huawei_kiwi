3da8b33b315b41c57eb07bf6ddee05fbf5828efe
Michael W <baddaemon87@gmail.com>
2018-06-16 10:07:33 +0200

    kiwi: Fix build
    * These have been renamed, so follow their paths
    
    Change-Id: Ia002d3842cc99274ddcea7ef4a01cc3b5458954d

--- lineage.mk
+++ lineage.mk
@@ -15,7 +15,7 @@
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_kiwi
 BOARD_VENDOR := huawei
