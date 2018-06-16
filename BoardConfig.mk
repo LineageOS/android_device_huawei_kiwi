9ee48804c87a1e5cdf4af6c45cea24fde91b5544
Michael W <baddaemon87@gmail.com>
2018-06-16 13:04:34 +0200

    kiwi: Fix shims
    * Instead of a global shim to libcutils, shim the stuff
      that actually needs it
    
    Change-Id: I18b55e4be5223a8f650a5e12ca715ad65975d1b3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -162,9 +162,10 @@ BOARD_SEPOLICY_DIRS += \
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
+    /system/bin/mm-qcamera-daemon|libshim_cutils.so:\
     /system/bin/signinfolistener|libshim_signinfolistener.so:\
-    /system/lib/libcutils.so|libshim_cutils.so:\
-    /system/lib64/libcutils.so|libshim_cutils.so:\
+    /system/vendor/bin/thermal-engine|libshim_cutils.so:\
+    /system/vendor/lib/hw/camera.msm8916.so|libshim_cutils.so:\
     /system/vendor/lib/libmmcamera_hdr_gb_lib.so|/system/vendor/lib/libmmqjpeg_codec.so
 
 # Vendor Init
