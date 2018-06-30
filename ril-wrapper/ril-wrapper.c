cdad63d2cf42116f15617ebfad87e4bcda861351
Dobroslaw Kijowski <dobo90@gmail.com>
2018-06-30 17:58:12 +0200

    kiwi: report signal strength properly
    * Signal strength logic based on following commits:
    
    Change-Id: I29e0a5b520a3dfa6edd888037222cfd7d96153ab
    kiwi: ril: Scale gsmSignalStrength properly
    kiwi: ril: Prioritize wcdma signal strength over gsm signal strength
    kiwi: ril: Add custom SignalStrength parcel handling

--- ril-wrapper/ril-wrapper.c
+++ ril-wrapper/ril-wrapper.c
@@ -28,6 +28,20 @@
 
 #define RIL_LIB_NAME "libril-qc-qmi-1.so"
 
+typedef struct {
+    int rscp;
+    int ecio;
+} RIL_WCDMA_SignalStrength;
+
+typedef struct {
+    RIL_GW_SignalStrength GW_SignalStrength;
+    RIL_WCDMA_SignalStrength WCDMA_SignalStrength;
+    RIL_CDMA_SignalStrength CDMA_SignalStrength;
+    RIL_EVDO_SignalStrength EVDO_SignalStrength;
+    RIL_LTE_SignalStrength_v8 LTE_SignalStrength;
+    RIL_TD_SCDMA_SignalStrength TD_SCDMA_SignalStrength;
+} RIL_SignalStrength_v10_vendor;
+
 /*
  * These structs are only avaiable in ril_internal.h
  * which is not exposed.
@@ -49,6 +63,28 @@ typedef struct RequestInfo {
 static const RIL_RadioFunctions* qmiRilFunctions;
 static const struct RIL_Env* ossRilEnv;
 
+static size_t transformResponse(const void* response) {
+    RIL_SignalStrength_v10_vendor vendorResponse = *(RIL_SignalStrength_v10_vendor*)response;
+    RIL_SignalStrength_v10* rilResponse = (RIL_SignalStrength_v10*)response;
+    int gsmSignalStrength = vendorResponse.GW_SignalStrength.signalStrength;
+
+    if (gsmSignalStrength != -1) {
+        gsmSignalStrength = -(gsmSignalStrength - 113) / 2;
+    }
+
+    rilResponse->GW_SignalStrength.signalStrength = vendorResponse.WCDMA_SignalStrength.rscp <= 0
+                                                        ? gsmSignalStrength
+                                                        : vendorResponse.WCDMA_SignalStrength.rscp;
+    rilResponse->GW_SignalStrength.bitErrorRate = vendorResponse.GW_SignalStrength.bitErrorRate;
+
+    rilResponse->CDMA_SignalStrength = vendorResponse.CDMA_SignalStrength;
+    rilResponse->EVDO_SignalStrength = vendorResponse.EVDO_SignalStrength;
+    rilResponse->LTE_SignalStrength = vendorResponse.LTE_SignalStrength;
+    rilResponse->TD_SCDMA_SignalStrength.rscp = vendorResponse.WCDMA_SignalStrength.rscp;
+
+    return sizeof(RIL_SignalStrength_v10);
+}
+
 static void onRequestCompleteShim(RIL_Token t, RIL_Errno e, void* response, size_t responselen) {
     if (!response) {
         ALOGV("%s: response is NULL", __func__);
@@ -62,10 +98,40 @@ static void onRequestCompleteShim(RIL_Token t, RIL_Errno e, void* response, size
     }
 
     int request = requestInfo->pCI->requestNumber;
-    switch (request) {}
+    switch (request) {
+        case RIL_REQUEST_SIGNAL_STRENGTH:
+            if (responselen != sizeof(RIL_SignalStrength_v10_vendor)) {
+                ALOGE("%s: invalid response length", __func__);
+                goto do_not_handle;
+            }
+
+            responselen = transformResponse(response);
+            break;
+    }
 
 do_not_handle:
     ossRilEnv->OnRequestComplete(t, e, response, responselen);
+}
+
+static void onUnsolicitedResponseShim(int unsolResponse, const void* data, size_t datalen) {
+    if (!data) {
+        ALOGV("%s: data is NULL", __func__);
+        goto do_not_handle;
+    }
+
+    switch (unsolResponse) {
+        case RIL_UNSOL_SIGNAL_STRENGTH:
+            if (datalen != sizeof(RIL_SignalStrength_v10_vendor)) {
+                ALOGE("%s: invalid response length", __func__);
+                goto do_not_handle;
+            }
+
+            datalen = transformResponse(data);
+            break;
+    }
+
+do_not_handle:
+    ossRilEnv->OnUnsolicitedResponse(unsolResponse, data, datalen);
 }
 
 const RIL_RadioFunctions* RIL_Init(const struct RIL_Env* env, int argc, char** argv) {
@@ -83,6 +149,7 @@ const RIL_RadioFunctions* RIL_Init(const struct RIL_Env* env, int argc, char** a
      */
     shimmedRilEnv = *env;
     shimmedRilEnv.OnRequestComplete = onRequestCompleteShim;
+    shimmedRilEnv.OnUnsolicitedResponse = onUnsolicitedResponseShim;
 
     /*
      * Open the qmi RIL.
