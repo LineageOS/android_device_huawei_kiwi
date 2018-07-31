9f471adc1d86645181c1333f48083601a386982c
Nicholas Lim <nicknitewolf@lineageos.org>
2018-07-31 22:17:27 +0800

    kiwi: Fix HWC1 support with surfaceflinger
    TARGET_USES_HWC2 is now assumed to be always true.
    https://android.googlesource.com/platform/frameworks/native/+/d265c4238699ea899cc3b01cc90b643d9692db2a
    Add android.hardware.graphics.composer@2.1-impl and
    the respective manifest.xml change to use the HWC2on1
    adapter.
    
    Change-Id: Iaeed042e7ea382d156137511460c460c6d6e3cf9

--- device.mk
+++ device.mk
@@ -111,6 +111,7 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
