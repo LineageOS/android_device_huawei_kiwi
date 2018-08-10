f2d746f2d0d9db3546f91a2a4d7d0d57a3bb0faa
YumeMichi <do4suki@gmail.com>
2018-08-10 10:15:11 +0800

    kiwi: recovery_updater: Update for P compatability
    librecovery_updater_kiwi: C_INCLUDES must be under the source or output
    directories: /edify/include /otautil/include /updater/include.
    
    Change-Id: I43216d9a051dd22dfeb926d49b86def7dc3bb4c1
    Signed-off-by: YumeMichi <do4suki@gmail.com>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,7 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_C_INCLUDES := $(call project-path-for,recovery)
+
+LOCAL_C_INCLUDES := \
+    bootable/recovery \
+    bootable/recovery/edify/include \
+    bootable/recovery/otautil/include \
+    bootable/recovery/updater/include
+
 LOCAL_SRC_FILES := recovery_updater.cpp
 LOCAL_MODULE := librecovery_updater_kiwi
 LOCAL_MODULE_TAGS := eng
