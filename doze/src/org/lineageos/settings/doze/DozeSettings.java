5d8137d78bfa73f160d009bcf313075c4975615d
Michael W <baddaemon87@gmail.com>
2018-08-20 17:26:25 +0200

    kiwi: Rebrand doze
    Change-Id: If9a7fe3dd39f107b1922c7e1859414974e8b5bf8

--- doze/src/org/lineageos/settings/doze/DozeSettings.java
+++ doze/src/org/lineageos/settings/doze/DozeSettings.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cyanogenmod.settings.doze;
+package org.lineageos.settings.doze;
 
 import android.os.Bundle;
 import android.view.MenuItem;
--- doze/src/org/lineageos/settings/doze/SensorsDozeService.java
+++ doze/src/org/lineageos/settings/doze/SensorsDozeService.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package com.cyanogenmod.settings.doze;
+package org.lineageos.settings.doze;
 
 import android.app.Service;
 import android.content.BroadcastReceiver;
@@ -35,7 +35,7 @@ import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
 
-import cyanogenmod.providers.CMSettings;
+import lineageos.providers.LineageSettings;
 
 public class SensorsDozeService extends Service {
 
@@ -316,8 +316,8 @@ public class SensorsDozeService extends Service {
                 Context.AUDIO_SERVICE);
         Vibrator vibrator = (Vibrator) mContext.getSystemService(Context.VIBRATOR_SERVICE);
 
-        boolean enabled = CMSettings.System.getInt(mContext.getContentResolver(),
-                CMSettings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, 1) != 0;
+        boolean enabled = LineageSettings.System.getInt(mContext.getContentResolver(),
+                LineageSettings.System.TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK, 1) != 0;
 
         switch (audioManager.getRingerMode()) {
             case AudioManager.RINGER_MODE_SILENT:
