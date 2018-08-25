64306f203e222e8cc2c452778b9f04896186383d
dianlujitao <dianlujitao@lineageos.org>
2018-08-25 19:07:05 +0800

    kiwi: Rename bt props for new HAL
    Change-Id: I1afd110bffed0f43b1afcbef99be8a3776d24e0b

--- rootdir/bin/init.qcom.bt.sh
+++ rootdir/bin/init.qcom.bt.sh
@@ -51,7 +51,7 @@ failed ()
 POWER_CLASS=`getprop qcom.bt.dev_power_class`
 LE_POWER_CLASS=`getprop qcom.bt.le_dev_pwr_class`
 
-setprop bluetooth.status off
+setprop vendor.bluetooth.status off
 
 case $POWER_CLASS in
   1) PWR_CLASS="-p 0" ;
@@ -83,11 +83,11 @@ case $exit_code_hci_qcomm_init in
   0) logi "Bluetooth QSoC firmware download succeeded, $BTS_DEVICE $BTS_TYPE $BTS_BAUD $BTS_ADDRESS";;
   *) failed "Bluetooth QSoC firmware download failed" $exit_code_hci_qcomm_init;
 
-     setprop bluetooth.status off
+     setprop vendor.bluetooth.status off
 
      exit $exit_code_hci_qcomm_init;;
 esac
 
-setprop bluetooth.status on
+setprop vendor.bluetooth.status on
 
 exit 0
