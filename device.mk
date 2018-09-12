4c21b50b34ddccdc9b06eed0fb642b7c3405bf85
joelh <joelh@crdroid.net>
2018-09-12 15:54:09 -0400

    kiwi: Build binderized omx hal
    Change-Id: Icd27c696fe0adb0b89809382444e6bedeb4eafb1

--- device.mk
+++ device.mk
@@ -194,6 +194,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml
 
 PRODUCT_PACKAGES += \
+    android.hardware.media.omx@1.0-impl \
+    android.hardware.media.omx@1.0-service \
     libdashplayer \
     libOmxAacEnc \
     libOmxAmrEnc \
