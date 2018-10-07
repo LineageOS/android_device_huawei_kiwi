d02ef62b8be1581c2678b45e653a2c494122dcd1
Bruno Martins <bgcngm@gmail.com>
2018-10-08 00:25:18 +0100

    kiwi: Create root folders outside of vendor init script
     * The core SELinux policies for vendor_init are being restricted as to
       comply with Treble. Simply adding a local rule to allow creating
       folders under rootfs would be enough, but at the same time defining
       the extra root folders and making use of AID/fs configuration file
       to set perms is more neat and clean.
    
    Change-Id: Idabd7ae72e4c4fd9daac5ba3be3e6236f79f251b

--- device.mk
+++ device.mk
@@ -157,6 +157,10 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
+# For config.fs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
