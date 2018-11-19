01b1514a72ec96a403c42c0e337c9e67fc749aed
Lyudmila17 <merkel.naller@gmail.com>
2018-11-19 22:10:29 +0100

    kiwi: Fix FM radio
    * Revert FM prop rename and add old BT SoC prop
    
    This reverts commit b2242c23a51c913b4d578ae55bcf29a40fe8096d.
    
    [@ghostrider-reborn: Also set proper permissions to the sysfs node]
    
    Change-Id: I5bea9bc407b0099320ca84edd6cadb01dd520215
    Co-authored-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- device.mk
+++ device.mk
@@ -60,7 +60,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
     audio.a2dp.default \
     audio.primary.msm8916 \
     audio.r_submix.default \
