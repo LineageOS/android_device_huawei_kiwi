34dd80313cc9cfce8a727e9203af81bd9f4ee0ce
Michael W <baddaemon87@gmail.com>
2018-11-25 14:15:50 +0100

    kiwi: Update devfreq entries
    * The references paths in the shell script don't exist
    * Instead they are symlinked to /sys/class/devfreq/[...]
    * Due to that, it's not required to set them via wildcard
    * -> Can get rid of the script and just set them normally
    * -> Can get rid of the sepolicies for the script
    
    * This change is based on the findings of joelh <joelh@cdroid.net>
    
    Change-Id: Ic766bbd81e6e37c2bbe7c0fc143a87fd9fa3b341

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -42,14 +42,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.power.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := bin/init.qcom.power.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
