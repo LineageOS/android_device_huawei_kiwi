37c7886ce572be711c1ddd84a75a8f9eb3ba056b
joelh <joelhans@protonmail.com>
2018-12-02 21:37:06 -0500

    kiwi: Binderize bluetooth hal
    Change-Id: I0a7628224facbb81b7a660cd75e06fb8c324b31e

--- device.mk
+++ device.mk
@@ -89,6 +89,7 @@ PRODUCT_PACKAGES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service \
     libbt-vendor \
     init.qcom.bt.sh
 
