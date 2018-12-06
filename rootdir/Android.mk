LineageOS/android_device_cyanogen_msm8916-common

rootdir: move shell files to /system/vendor/bin
Change-Id: I45ccd3ce8a1f4661d5b3a3cf886c9bb5b86cb730

rootdir/Android.mk
@@ -6,7 +6,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.mem.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.mem.sh
+LOCAL_SRC_FILES    := bin/init.qcom.mem.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 # Init scripts

LineageOS/android_device_huawei_kiwi

rootdir/Android.mk
@@ -5,8 +5,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.class_main.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.class_main.sh
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_SRC_FILES    := bin/init.class_main.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -45,8 +45,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.power.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := etc/init.qcom.power.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
+LOCAL_SRC_FILES    := bin/init.qcom.power.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)