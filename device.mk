1fcb7f34b1b9708a00a5f606e1d1e2b84ea899dc
Danny Baumann <dannybaumann@web.de>
2019-01-25 08:55:33 +0100

    kiwi: Use livedisplay@2.0 HAL
    Change-Id: I5ba6578e7687601d82e36b0bac630d965a1fc66d

--- device.mk
+++ device.mk
@@ -194,7 +194,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@1.0-service-legacymm
+    vendor.lineage.livedisplay@2.0-service-legacymm
 
 # Media
 PRODUCT_COPY_FILES += \
