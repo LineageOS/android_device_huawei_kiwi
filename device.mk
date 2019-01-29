cefe526481b46c9fd2da2fcdd6e4e2417d76dc93
joelh <joelhans@protonmail.com>
2019-01-29 17:34:14 -0500

    kiwi: Switch to prebuilt libantradio
    * Pin blobs and add build rule
    
    Change-Id: Ie6f047d718ba018d55b8fd62fd141a06db66ca62

--- device.mk
+++ device.mk
@@ -81,8 +81,7 @@ PRODUCT_COPY_FILES += \
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \
-    com.dsi.ant.antradio_library \
-    libantradio
+    com.dsi.ant.antradio_library
 
 # Bluetooth
 PRODUCT_PACKAGES += \
