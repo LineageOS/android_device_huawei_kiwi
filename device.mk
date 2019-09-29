640744d255ce46317626f3af3ae3734380c0bcd5
Davide Garberi <dade.garberi@gmail.com>
2019-09-29 11:59:57 +0200

    kiwi: Switch to powerhal 1.2
    Change-Id: I2d5d4b24def70a027a04fc81fa168fb698280a98

--- device.mk
+++ device.mk
@@ -238,7 +238,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Radio
 PRODUCT_PACKAGES += \
