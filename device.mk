671f9dc045b53ff856f07c6a26501b7ab53c2944
Bruno Martins <bgcngm@gmail.com>
2019-10-04 23:20:42 +0100

    kiwi: Switch to legacy Wi-Fi service
    Change-Id: I690bc80349e47907a29a89b8a6f96659f1c61aba

--- device.mk
+++ device.mk
@@ -311,7 +311,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     libwpa_client \
     wificond \
     hostapd \
